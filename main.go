d084c74b9120273b851ccde46e4b16f650a7bbc9
grsakea <grsakea@users.noreply.github.com>
2017-12-09 19:20:50 +0100

    Add first feature

--- main.go
+++ main.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/grsakea/go-twitch"
+)
+
+var s = twitch.NewSession()
+
+type Config struct {
+	Streamers []string `json:"streamers"`
+}
+
+func main() {
+	conf, err := loadConfig("config.json")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for i := 0; i < len(conf.Streamers); i++ {
+		onl, err := isOnline(conf.Streamers[i])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		fmt.Println(conf.Streamers[i], onl)
+	}
+}
+
+func loadConfig(path string) (Config, error) {
+	var conf Config
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+	err = json.Unmarshal(dat, &conf)
+	if err != nil {
+		return Config{}, err
+	}
+	return conf, nil
+}
+
+func isOnline(name string) (bool, error) {
+	filter := twitch.GetStreamInput{}
+	filter.UserLogin = name
+	data, err := s.GetStream(&filter)
+	if err != nil {
+		return false, err
+	}
+	return len(data.Data) == 1, nil
+}
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestLoadConfig(t *testing.T) {
+	conf, err := loadConfig("fixtures/config.json")
+	if err != nil {
+		t.Error("Error during conf parsing, ", err)
+	}
+	if len(conf.Streamers) != 1 {
+		t.Error("Invalid number of streamer")
+	}
+	if conf.Streamers[0] != "twitch" {
+		t.Error("Conf parsing failed")
+	}
+}
+
+func TestLoadAbsentConfig(t *testing.T) {
+	_, err := loadConfig("fixtures/invalid_config.json")
+	if err == nil {
+		t.Error("Should have been an error")
+	}
+}
