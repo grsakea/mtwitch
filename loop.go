bb03eab5dab9ed7caa0c6b28f188326ce4540c99
grsakea <grsakea@users.noreply.github.com>
2017-12-19 00:18:14 +0100

    Improve stream naming

--- loop.go
+++ loop.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"regexp"
 	"time"
 
 	twitch "github.com/grsakea/go-twitch"
@@ -17,21 +18,35 @@ func followStream(channels []string, s twitch.Interface) {
 
 func startRecord(channel string, s twitch.Interface) {
 	log.Println("start", channel)
-	state := false
 	for {
-		newState, err := isOnline(channel, s)
+		state, err := isOnline(channel, s)
 		if err != nil {
 			log.Println("error :", err)
-		} else if state != newState {
-			state = newState
-			if state {
-				a, _ := s.ExtractStreamUrl(channel)
-				hls.Download(a[0].URL, "/var/tmp/"+channel)
-			}
+		} else if state {
+			recordStream(channel, s)
+			log.Println("Stopping recording of", channel)
 		}
 
 		time.Sleep(30 * time.Second)
 	}
+}
+
+func recordStream(channel string, s twitch.Interface) {
+	log.Println("Starting recording of", channel)
+	st, _ := s.GetStream(twitch.GetStreamInput{UserLogin: channel})
+	stURL, _ := s.ExtractStreamUrl(channel)
+	filename := streamFilename(st.Data[0])
+	hls.Download(stURL[0].URL, filename)
+}
+
+func streamFilename(s twitch.Stream) string {
+	t := time.Now()
+
+	outTime := t.Format("06-01-02_15:04")
+	re := regexp.MustCompile(`[ \|\!|@]+`)
+	outChan := re.ReplaceAllString(s.Title, "_")
+
+	return outTime + "-" + outChan + ".mp4"
 }
 
 func channelStatus(channel string, state bool) string {
